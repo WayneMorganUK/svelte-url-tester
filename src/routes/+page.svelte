8cc8a3efef38703a49cc7a48b473ce3007d45b23
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2025-11-16 12:08:51 +0000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Sample from './Sample.svelte';
+	let url = 'https://example.com' as const;
+	let newUrl: URL = new URL('https://newexample.com');
+</script>
+
+<Sample {url} {newUrl} />
---
+++ src/routes/Sample.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { url, newUrl }: { url: string; newUrl: URL } = $props();
+	let homepage: string = 'https://example.com';
+</script>
+
+<a href={url}>Visit Example</a>
+
+<a href={newUrl.href}>Visit Example</a>
+
+<a href="https://example.com">Visit Example</a>
+
+<a href={homepage}>Visit Homepage</a>
